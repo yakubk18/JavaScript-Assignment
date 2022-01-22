3f9383d7a86e17ee6191ef60f932a5f5ff40094f
yakubk18 <golandazyakub@gmail.com>
2022-01-22 11:12:04 +0530

    initail commit for play with array

--- assignments/play_with_array/play_with_array.js
+++ assignments/play_with_array/play_with_array.js
@@ -7,6 +7,13 @@ function getEven(arr) {
 
         Write you code below
     */
+   var arr1=[];
+   for (var i=0;i<arr.length;i++){
+       if (arr[i]%2==0){
+           arr1.push(arr[i])
+       }
+   }
+   return arr1
 
 }
 
@@ -18,6 +25,12 @@ function multiplyByN(arr, n) {
             Output: [3,9,13,165]
         Write you code below
     */
+   var arr1=[];
+   for(var i=0;i<arr.length;i++){
+       arr[i]=arr[i]*n;
+       arr1.push(arr[i])
+   }
+   return arr1
 }
 
 function removeNthElement(arr, n) {
@@ -28,6 +41,8 @@ function removeNthElement(arr, n) {
             Output: [1,3,4,7]
         Write you code below
     */
+   arr.splice(n,1);
+   return arr;
 }
 
 module.exports = {
