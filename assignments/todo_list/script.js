8ebb69c26f8edccc940be78eb3e112d0a0700335
yakubk18 <golandazyakub@gmail.com>
2022-02-07 09:25:35 +0530

    todo list with Api

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,19 +1,57 @@
-var input=document.querySelector('input');
-var button=document.querySelector('button');
-var todoDiv=document.querySelector('div.todos');
+let todos = [];
+var addToDoButton = document.querySelector('button');
+// var inputfield = document.querySelector('input');
+var todo = document.querySelector('.todos')
+// var i = 0;
+let drop = document.querySelector("#dropdown");
+addToDoButton.addEventListener("click", work);
 
-let count=0;
 
-button.addEventListener('click', function(){
-    console.log(input.value);
-    if(input.value) {
-        var newTodo=document.createElement('p');
-        newTodo.innerHTML=input.value;
-        newTodo.setAttribute('key',count++);
-        todoDiv.appendChild(newTodo);
-        newTodo.addEventListener('click',function(e){
-            todoDiv.removeChild(e.target);
+async function main() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const json = await response.json();
+        todos = json.slice(0, 10);
+        console.log(todos);
+        todos.forEach((todom,index)=>{
+            const newTodo = document.createElement('p');
+            newTodo.setAttribute('key', index);
+            newTodo.innerHTML = todom.title
+            todo.appendChild(newTodo);
         })
-        input.value="";
+    }catch(e) {
+        console.log(e);
     }
-})
+}
+
+function work(){
+    if(drop.value ==="list"){
+        main()
+    }else if(drop.value === "completed"){
+        completed()
+    }else if(drop.value === "pending"){
+        pending()
+    }
+}
+
+function completed(){
+    todo.innerHTML = ""
+    todos.filter(todom => todom.completed).forEach((todom,index)=>{
+        const newTodo = document.createElement('p')
+        newTodo.setAttribute('key',index)
+        newTodo.innerHTML = todom.title
+        todo.appendChild(newTodo);
+    })
+
+}
+
+function pending(){
+    todo.innerHTML = ""
+    todos.filter(todom => !todom.completed).forEach((todom,index)=>{
+        const newTodo = document.createElement('p')
+        newTodo.setAttribute('key',index)
+        newTodo.innerHTML = todom.title
+        todo.appendChild(newTodo);
+    })
+}
+main()
